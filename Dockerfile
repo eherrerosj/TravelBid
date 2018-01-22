8bc03d1e6a24c2751db23347fa9a7fc39023d52c
Enrique Herreros <enrique@findhotel.net>
2018-01-22 12:28:12 +0100

    Patch applied from Alberto

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM python:3.6.1-slim
 RUN apt-get update && apt-get install -y git build-essential
 
-EXPOSE 5000
-
 COPY requirements.txt ./
 
 RUN pip install -r requirements.txt
@@ -10,4 +8,4 @@ RUN pip install -r requirements.txt
 COPY app.py utils.py ./
 ADD data ./data
 
-CMD ["python", "app.py"]
+ENTRYPOINT ["python", "app.py"]
--- app.py
+++ app.py
@@ -4,10 +4,6 @@ import utils
 app = Flask(__name__)
 
 HOTELS_CSV_PATH = "./data/hotels.csv"
-HEADER = {
-    "Content-Type": "application/json",
-    "User-Agent": "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
-}
 
 
 @app.route('/candidates')
@@ -35,4 +31,4 @@ def start():
 
 if __name__ == '__main__':
     start()
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
